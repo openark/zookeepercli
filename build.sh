9dbfc99db90595102c39ec48e9d50b3d4219e2e1
shlomi-noach <shlomi@openark.org>
2015-06-16 10:00:41 +0200

    versio update

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Requires fpm: https://github.com/jordansissel/fpm
 #
 
-release_version="1.0.8"
+release_version="1.0.9"
 release_dir=/tmp/zookeepercli
 rm -rf $release_dir/*
 mkdir -p $release_dir
